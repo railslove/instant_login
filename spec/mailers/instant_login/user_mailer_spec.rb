e310c1160aea9805c6baf3c63abf43672ded630f
Maximilian Schulz <m.schulz@kulturfluss.de>
2014-11-05 18:45:52 +0100

    Fix broken spec

--- spec/mailers/instant_login/user_mailer_spec.rb
+++ spec/mailers/instant_login/user_mailer_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 module InstantLogin
-  RSpec.describe Login, :type => :mailer do
+  RSpec.describe UserMailer, :type => :mailer do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
